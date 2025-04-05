a3f57cb4578b156c6a0940d96839f44036ce49af
Gurman Singh <77499829+gurman007@users.noreply.github.com>
2025-04-05 02:22:24 -0500

    Add files via upload

---
+++ EffiLLM-Project/2_problems/easy/dp_004/human.py
@@ -0,0 +1,9 @@
+# Problem: Pascal
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        f = [[1]]
+        for i in range(numRows - 1):
+            g = [1] + [a + b for a, b in pairwise(f[-1])] + [1]
+            f.append(g)
+        return f
---
+++ EffiLLM-Project/2_problems/medium/dp_010/human.py
@@ -0,0 +1,8 @@
+# Problem: Maximum Alternating Subsequence Sum
+
+class Solution:
+    def maxAlternatingSum(self, nums: List[int]) -> int:
+        f = g = 0
+        for x in nums:
+            f, g = max(g - x, f), max(f + x, g)
+        return max(f, g)
